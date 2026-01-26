e5de805ae95be0d2709d595f14ef9831192c3e1c
Shehryar Aijaz <shehryar120345@gmail.com>
2026-01-26 05:21:07 +0500

    created home page and added elysia for api routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,12 @@
 import type { Metadata } from "next";
-import { jetbrainsMono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const jetbrainsMono = jetbrainsMono({
+const jetbrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
   subsets: ["latin"],
 
 })
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
