96cda25d8aea39d98aae9845a5be184da362f6f3
Shehryar Aijaz <shehryar120345@gmail.com>
2026-01-27 04:58:38 +0500

    feat: enhance user experience with error handling and loading state in room creation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,113 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import { useMutation } from "@tanstack/react-query";
 import { client } from "@/lib/client";
-import { useRouter } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
+
+const errorMessages: Record<string, string> = {
+  "room-not-found-404": "The room does not exist.",
+  "room-full": "This room is already full.",
+}
 
 export default function Home() {
+  const [username, setUsername] = useState<string | null>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
 
-  const [username, setUsername] = useState<string | null>('');
-  const router = useRouter()
+  const params = useSearchParams();
+  const error = params.get("error");
 
-  const ANIMALS = ['penguin', 'wolf', 'leapord', 'cat', 'dinasour']
+  const ANIMALS = ["penguin", "wolf", "leapord", "cat", "dinasour"];
 
   const randomWord = async () => {
-    const response = await fetch(`https://random-word-api.herokuapp.com/word`)
+    const response = await fetch(`https://random-word-api.herokuapp.com/word`);
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
     const word = await response.json();
     return word[0];
-  }
+  };
 
   const generateUsername = (word: string) => {
-    const username = ANIMALS[Math.floor(Math.random() * ANIMALS.length)]
-    return `${word}-${username}-${nanoid(5)}`
-  }
+    const username = ANIMALS[Math.floor(Math.random() * ANIMALS.length)];
+    return `${word}-${username}-${nanoid(5)}`;
+  };
 
-  const STORAGE = "custom-username"
+  const STORAGE = "custom-username";
 
   useEffect(() => {
-    const initUsername = async () => {    
-      const storedUsername = localStorage.getItem(STORAGE)
+    const initUsername = async () => {
+      const storedUsername = localStorage.getItem(STORAGE);
       if (storedUsername) {
-        setUsername(storedUsername)
+        setUsername(storedUsername);
         return;
       }
 
-      const word = await randomWord()
-      const getUsername = generateUsername(word)
-      localStorage.setItem(STORAGE, getUsername)
-      setUsername(getUsername)
-  }
+      const word = await randomWord();
+      const getUsername = generateUsername(word);
+      localStorage.setItem(STORAGE, getUsername);
+      setUsername(getUsername);
+    };
 
-  initUsername();
-  }, [])
+    initUsername();
+  }, []);
 
   const { mutate: createRoom } = useMutation({
     mutationFn: async () => {
-      const res = await client.room.create.post()
-      
+      setLoading(true);
+      const res = await client.room.create.post();
+
       if (res.status === 200) {
-        router.push(`/room/${res.data?.roomId}`)
+        router.push(`/room/${res.data?.roomId}`);
       }
-    }
-  })
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    },
+  });
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4">
       <div className="w-full max-w-md space-y-8">
-
         <div className="text-center space-y-2">
-          <h1 className="text-2xl font-bold tracking-tight text-green-500"><span className="animate-pulse">{">"}</span>private_chat</h1>
-          <p className="text-zinc-500 text-sm">private, self-destructing chat room.</p>
+          <h1 className="text-2xl font-bold tracking-tight text-green-500">
+            <span className="animate-pulse">{">"}</span>private_chat
+          </h1>
+          <p className="text-zinc-500 text-sm">
+            private, self-destructing chat room.
+          </p>
         </div>
 
         <div className="border border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
           <div className="space-y-5">
             <div className="space-y-2">
-              <label className="flex items-center text-zinc-500">Your Identity</label>
+              <label className="flex items-center text-zinc-500">
+                Your Identity
+              </label>
               <div className="flex items-center gap-3">
-                <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-zinc-400 font-mono">{username}</div>
+                <div className="flex-1 bg-zinc-950 border border-zinc-800 p-3 text-sm text-zinc-400 font-mono">
+                  {username}
+                </div>
               </div>
             </div>
           </div>
 
-          <button onClick={() => createRoom()} className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50">CREATE ROOM</button>
+          <button
+            onClick={() => createRoom()}
+            disabled={loading}
+            className="w-full bg-zinc-100 text-black p-3 text-sm font-bold hover:bg-zinc-50 hover:text-black transition-colors mt-2 cursor-pointer disabled:opacity-50"
+          >
+            {loading ? "CREATING ROOM.." : "CREATE ROOM"}
+          </button>
+        </div>
+
+        <div className="flex flex-col items-center">
+        {error && (
+          <p className="text-sm text-red-500">{errorMessages[error] ?? "Something went wrong. Please try again." }</p>
+        )}
         </div>
       </div>
     </div>
